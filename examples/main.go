591e8b5204f19eb451c5c3145d8c02480c0f4597
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-07-22 17:23:27 +0600

    first commit

---
+++ examples/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http/httputil"
+
+	"github.com/khulnasoft-labs/retryablehttp-go"
+)
+
+var (
+	url   string
+	short bool
+)
+
+func main() {
+	flag.StringVar(&url, "url", "https://scanme.sh", "URL to fetch")
+	flag.BoolVar(&short, "short", false, "Skip printing http response body")
+	flag.Parse()
+
+	opts := retryablehttp.DefaultOptionsSpraying
+	// opts := retryablehttp.DefaultOptionsSingle // use single options for single host
+	client := retryablehttp.NewClient(opts)
+	resp, err := client.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	bin, err := httputil.DumpResponse(resp, !short)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bin))
+}
